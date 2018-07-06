2b5e89e2411ae41784e561d5465a46223241dc98
loangelak <loangela@optonline.net>
2018-07-06 14:31:50 -0400

    Add files via upload

---
+++ ex34.py
@@ -0,0 +1,6 @@
+#https://github.com/chris-void/pyway/blob/master/Learn%20Python%20The%20Hard%20Way%2C%203rd%20Edition%20.pdf
+#Accessing Elements of Lists
+animals = ["bear", 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
