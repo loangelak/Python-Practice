44794c752d82ad6dd488c03dec7e5f0fec7011e5
loangelak <loangela@optonline.net>
2018-07-06 14:27:03 -0400

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called: ", script
+print "Your first variable is: ", first
+print "Your second variable is: ", second
+print "Your third variable is: ", third
+
+
+#python ex13.py stuff things that
+#python ex13.py apple orange grapefruit
