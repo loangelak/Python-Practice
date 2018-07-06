3f1b577da733deaa820425c7bc2ab335b7437e39
loangelak <loangela@optonline.net>
2018-07-06 14:34:24 -0400

    Add files via upload

---
+++ mystuff.py
@@ -0,0 +1,11 @@
+def apple(): 
+	print 'I AM APPLES!'
+
+#this is just a variable
+tangerine = "Living reflection of a dream."
+
+
+class MyStuff(object): 
+
+	def __init__(self):
+		self.tangerine = 'And now a thousand years between.'
